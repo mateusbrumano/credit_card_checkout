d71e5bbebff8c4a83131d232db56d3a2488a3c32
Mateus Brumano <mateus.castro@infosolo.com.br>
2018-07-16 02:18:32 -0300

    Background and positions adjust

--- src/App.vue
+++ src/App.vue
@@ -30,25 +30,25 @@
       </div>
       <div class="col-md-8">
         <div class="card w-100 h-100" id="cardInfo">
-          <form  class="card-body">
+          <form @submit="" class="card-body">
             <h3 class="card-title">Credit Card</h3>
             <div class="form-group">
               <label for="cardNumber">Card Number</label>
               <div class="row" id="cardNumber">
                 <div class="col-md-1">
-                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000">
+                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000" name="cardNumber1" id="cardNumber1" @change="checkCard()" v-model="cardNumber1">
                 </div>
                 <div class="col-md-1">
-                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000">
+                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000" name="cardNumber2" id="cardNumber2" @change="checkCard()" v-model="cardNumber2">
                 </div>
                 <div class="col-md-1">
-                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000">
+                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000" name="cardNumber3" id="cardNumber3" @change="checkCard()" v-model="cardNumber3">
                 </div>
                 <div class="col-md-1">
-                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000">
+                  <input type="text" maxlength="4" class="form-control text-center" placeholder="0000" name="cardNumber4" id="cardNumber4" @change="checkCard()" v-model="cardNumber4">
                 </div>
                 <div class="col-md-1">
-                  <span>visa/master img</span>
+                  <img v-bind:src="cardSimbol.url" v-bind:alt="cardSimbol.title">
                 </div>
               </div>
             </div>
@@ -98,7 +98,7 @@
                 <button type="button" class="btn btn-light">Pay With Order</button>
               </div>
               <div class="col-md-4">
-                <button type="button" class="btn btn-success">Checkout</button>
+                <button type="submit" value="Submit" class="btn btn-success">Checkout</button>
               </div>
             </div>
           </form>
@@ -113,11 +113,87 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      cardNumber1: '',
+      cardNumber2: '',
+      cardNumber3: '',
+      cardNumber4: '',
+      cardSimbol: {
+        url: '',
+        title: ''
+      }
     }
   },
   computed: {
 
+  },
+  methods: {
+    checkCard:function() {
+
+      let card = this.cardNumber1 + this.cardNumber2 + this.cardNumber3 + this.cardNumber4;
+
+      let cardAmex = /^(?:3[47])$/;
+      let cardVisa = /^(?:4[0-9])$/;
+      let cardMaster = /^(?:5[1-5])$/;
+
+      //Find a good logo and remove invalid credit card alert (Add css validation)
+
+      if (card.substring(0,2).match(cardAmex)) {
+        this.cardSimbol.url = 'https://fotw.info/images/u/us$amex.gif';
+        this.cardSimbol.title = 'America Express';
+      } else if (card.substring(0,2).match(cardVisa)) {
+        this.cardSimbol.url = 'https://logodownload.org/wp-content/uploads/2016/10/visa-logo-21.png';
+        this.cardSimbol.title = 'Visa';
+      } else if (card.substring(0,2).match(cardMaster)) {
+        this.cardSimbol.url = 'https://logodownload.org/wp-content/uploads/2014/07/mastercard-logo-7-1.png';
+        this.cardSimbol.title = 'MasterCard';
+      } else {
+        alert("invalid card")
+      }
+
+      this.checkLuhn(card)
+
+      if(!this.checkLuhn(card)) {
+        alert('Sorry, that is not a valid credit card number - please try again!');
+      }
+
+    },
+    checkLuhn:function (card) {
+      let sum = 0;
+      let numdigits = card.length;
+      let parity = numdigits % 2;
+      for(let i=0; i < numdigits; i++) {
+        var digit = parseInt(card.charAt(i))
+        if(i % 2 == parity) digit *= 2;
+        if(digit > 9) digit -= 9;
+        sum += digit;
+      }
+      return (sum % 10) == 0;
+    }
+
+
+    // checkCard:function(name) {
+    //   // var cardAmex = /^(?:3[47][0-9]{13})$/;
+    //   var cardAmex = /^(?:3[47])$/;
+    //   // var cardVisa = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+    //   var cardVisa = /^(?:4[0-9])$/;
+    //   // var cardMaster = /^(?:5[1-5][0-9]{14})$/;
+    //   var cardMaster = /^(?:5[1-5])$/;
+    //
+    //   // alert(name.length);
+    //
+    //   // if (name.length >= 3) {
+    //   //   alert('maior que 3');
+    //   if (name.substring(0,2).match(cardAmex)) {
+    //     alert("amex");
+    //   } else if (name.substring(0,2).match(cardVisa)) {
+    //     alert("visa");
+    //   } else if (name.substring(0,2).match(cardMaster)) {
+    //     alert("master");
+    //   } else {
+    //     alert("invalid card")
+    //   }
+    //   // }
+    // }
   }
 }
 </script>
@@ -136,43 +212,7 @@ export default {
 }
 
 #cardInfo {
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
   color: $cardInfoLetter;
-  /*width: 80rem;*/
-  /*height: 30rem;*/
-  /*margin-top: 60px;*/
 }
 
-/*#cardNumber {*/
-  /*input {*/
-    /*text-align: center;*/
-  /*}*/
-/*}*/
-
-/*#cardCVC {*/
-  /*input {*/
-    /*text-align: center;*/
-  /*}*/
-/*}*/
-
-/*h1, h2 {*/
-  /*font-weight: normal;*/
-/*}*/
-
-/*ul {*/
-  /*list-style-type: none;*/
-  /*padding: 0;*/
-/*}*/
-
-/*li {*/
-  /*display: inline-block;*/
-  /*margin: 0 10px;*/
-/*}*/
-
-/*a {*/
-  /*color: #42b983;*/
-/*}*/
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate'
 
 import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(VeeValidate);
 
 new Vue({
   el: '#app',
